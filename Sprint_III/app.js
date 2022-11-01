12353512a0ea400f31ce16bb1968100c34928a40
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2022-11-01 18:19:22 -0300

    routes_added_from_app_to_router

--- Sprint_III/app.js
+++ Sprint_III/app.js
@@ -10,35 +10,7 @@ app.set('views' , __dirname + '/views'); //Se fija una ruta dinamica (con __dirn
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 
-//ESCRITORIO
-app.get('/' , (req,res)=>{
-    //res.render('desktop_principal.ejs')
-    res.send('estoy en Escritorio')
-})
 
-//DETAIL PRODUCT
-app.get('/detail_product', (req,res)=>{
-    //res.render('producto.ejs')
-    res.send('estoy en Detail Product')
-})
-
-//CART
-app.get('/cart', (req,res)=>{
-    //res.render('cart.ejs')
-    res.send('estoy en cart')
-})
-
-//INICIAR SESION
-app.get('/iniciar_sesion', (req,res)=>{
-    //res.render('iniciar_sesion.ejs')
-    res.send('estoy en Iniciar Sesion')
-})
-
-//CREAR CUENTA
-app.get('/crear_cuenta', (req,res)=>{
-    //res.render('crear_cuenta.ejs')
-    res.send('estoy en Crear Cuenta')
-})
 
 //Listen port
 app.listen(PORT , ()=> console.log('listening on port',PORT));
