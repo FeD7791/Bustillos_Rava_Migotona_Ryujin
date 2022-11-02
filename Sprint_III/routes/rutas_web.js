0f769c6c431633ee5c48287c650b422edb137f10
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2022-11-01 22:25:10 -0300

    web_routes_for_html_display_added

--- Sprint_III/routes/rutas_web.js
+++ Sprint_III/routes/rutas_web.js
@@ -1,33 +1,35 @@
 const express = require('express');
-const router = express.Router();
+const router_web = express.Router();
 
 
 //ESCRITORIO
-router.get('/' , (req,res)=>{
+router_web.get('/' , (req,res)=>{
     res.render('desktop_principal.ejs')
     
 })
 
 //DETAIL PRODUCT
-router.get('/detail_product', (req,res)=>{
+router_web.get('/detail_product', (req,res)=>{
     res.render('producto.ejs')
     
 })
 
 //CART
-router.get('/cart', (req,res)=>{
+router_web.get('/cart', (req,res)=>{
     res.render('cart.ejs')
     
 })
 
 //INICIAR SESION
-router.get('/iniciar_sesion', (req,res)=>{
+router_web.get('/iniciar_sesion', (req,res)=>{
     res.render('iniciar_sesion.ejs')
     
 })
 
 //CREAR CUENTA
-router.get('/crear_cuenta', (req,res)=>{
+router_web.get('/crear_cuenta', (req,res)=>{
     res.render('crear_cuenta.ejs')
     
 })
+
+module.exports = router_web
