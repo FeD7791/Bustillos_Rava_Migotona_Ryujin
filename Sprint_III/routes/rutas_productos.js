99e98340e314251efadc90ba345a158d91f209f5
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2022-11-01 22:28:04 -0300

    product_routes_added

---
+++ Sprint_III/routes/rutas_productos.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router_products = express.Router();
+//Se importan las constantes de controller
+const {getall,getone} = require('../controllers/controllers');
+
+//Rutas
+router_products.get('/products', getall);
+router_products.get('products/:id',getone);
+
+module.exports = router_products
+
+
