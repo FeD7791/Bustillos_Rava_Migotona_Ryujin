baa2be1e7e7eb1b8b78e5b30848b99a0d98e8fc1
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2022-11-04 12:20:58 -0300

    express_validator_implemented

--- Sprint_III/routes/rutas_web.js
+++ Sprint_III/routes/rutas_web.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router_web = express.Router();
 const bodyparser = require('body-parser');
-const { body, validationResult, check } = require('express-validator');
+const { check, validationResult} = require('express-validator');
 
-
+const urlencoded = bodyparser.urlencoded({extended:false})
 //ESCRITORIO
 router_web.get('/' , (req,res)=>{
     res.render('desktop_principal.ejs')
@@ -31,6 +31,36 @@ router_web.get('/iniciar_sesion', (req,res)=>{
 //CREAR CUENTA
 router_web.get('/crear_cuenta', (req,res)=>{
     res.render('crear_cuenta.ejs')
+    
+})
+
+router_web.post('/register', urlencoded ,[
+    check('nombre','Debe introducir un nombre de usuario con minimo de 2 letras')
+       .exists()
+       .isLength({min:2}),
+    check('apellido', 'Apellido debe contener 2 letras')   
+       .exists()
+       .isLength({min:2}),
+    check('correo','Correo debe ser valido')
+       .exists()
+       .isEmail(),
+    check('fecha_de_nacimiento','fecha de nacimiento debe ser valida')
+       .exists()
+       .isDate(),
+    check('contrasenia','Debe tener almenos 5 caracteres')
+       .exists()
+       .isLength({min:5})
+                
+
+
+
+],(req,res)=>{
+    const error = validationResult(req)
+    if(!error.isEmpty()){
+        return res.status(400).json({errors: error.array()});
+    }else{
+        res.json('verificado!')
+    }
     
 })
 
