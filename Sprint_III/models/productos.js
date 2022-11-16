dff910b0b77daafc716f0e35f25502332b5207f4
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2022-11-15 21:28:58 -0300

    creados_modelos_usuarios_and_productos

---
+++ Sprint_III/models/productos.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class productos extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  productos.init({
+    id: DataTypes.INTEGER,
+    nombre: DataTypes.STRING,
+    descripcion: DataTypes.STRING,
+    precio: DataTypes.DECIMAL
+  }, {
+    sequelize,
+    modelName: 'productos',
+  });
+  return productos;
+};
