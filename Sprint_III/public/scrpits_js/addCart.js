af9224b7e18717ce083a752fa7cb9e3ab6b4228c
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2023-01-02 17:26:07 -0300

    vuelta_al_disenio_original

--- Sprint_III/public/scrpits_js/addCart.js
+++ Sprint_III/public/scrpits_js/addCart.js
@@ -1,16 +1,50 @@
+///////////////////minus sign elements //////////////
 let less = document.getElementById("button_minus") 
+let less_1 = document.getElementById("button_minus_1") 
+let less_2 = document.getElementById("button_minus_2") 
+/////////////////////////////////////////////////////
 let mas = document.getElementById("button_plus") 
+let mas_1 = document.getElementById("button_plus_1") 
+let mas_2 = document.getElementById("button_plus_2") 
+/////////////////////////////////////////////////////
 let box = document.getElementById("display_box") 
+let box_1 = document.getElementById("display_box_1") 
+let box_2 = document.getElementById("display_box_2") 
+/////////////////////////////////////////////////////
 
 
-function incrementar(){
-    box.stepUp()
+function incrementar(box_element){
+    let box_0 = box_element
+    box_0.stepUp()
     //box.innerHTML = numero+1
 }
 
-function disminuir(){
-    box.stepDown()
+function disminuir(box_element){
+    let box_0 = box_element
+    box_0.stepDown()
 }
 
-mas.addEventListener("click",incrementar)
-less.addEventListener("click",disminuir)
+
+
+///////////////////////Event listener/////////////////////
+mas.addEventListener("click",function(){
+    incrementar(box)
+})
+less.addEventListener("click",function(){
+    disminuir(box)
+})
+////////////////////////////////////////////
+mas_1.addEventListener("click",function(){
+    incrementar(box_1)
+})
+less_1.addEventListener("click",function(){
+    disminuir(box_1)
+})
+/////////////////////////////////////////////
+mas_2.addEventListener("click",function(){
+    incrementar(box_2)
+})
+less_2.addEventListener("click",function(){
+    disminuir(box_2)
+})
+/////////////////////////////////////////////
