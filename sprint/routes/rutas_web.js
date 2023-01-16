ef6b624f20670deb3023dc49c928c0306026fafc
Federico Bustillos <jorgefedericobustillosrava@gmail.com>
2023-01-16 11:05:56 -0300

    redo

---
+++ sprint/routes/rutas_web.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router_web = express.Router();
+const bodyparser = require('body-parser');
+const bcrypt = require('bcrypt')
+const { check, validationResult} = require('express-validator');
+const {create_user, user_session_validator, middleware_cookie,deleteOne} = require('../controllers/controllers') 
+
+const urlencoded = bodyparser.urlencoded({extended:false})
+router_web.use(bodyparser.json())//INDISPENSABLE PARA HACER POST EN JSON
+//ESCRITORIO
+router_web.get('/' , (req,res)=>{
+    res.render('desktop_principal.ejs')
+    
+})
+
+//DETAIL PRODUCT
+router_web.get('/detail_product', (req,res)=>{
+    res.render('producto.ejs')
+    
+})
+
+//CART
+router_web.get('/cart', (req,res)=>{
+    res.render('cart.ejs')
+    
+})
+
+//INICIAR SESION
+router_web.get('/iniciar_sesion', (req,res)=>{
+    res.render('iniciar_sesion.ejs')
+    
+})
+
+//CREAR CUENTA
+router_web.get('/crear_cuenta', (req,res)=>{
+    res.render('crear_cuenta.ejs')
+    
+})
+//Validar formulario de creacion de cuenta
+router_web.post('/register',urlencoded,[
+    check('nombre','Debe introducir un nombre de usuario con minimo de 2 letras')
+       .exists()
+       .isLength({min:2}),
+    check('apellido', 'Apellido debe contener 2 letras')   
+       .exists()
+       .isLength({min:2}),
+    check('correo','Correo debe ser valido')
+       .exists()
+       .isEmail(),
+    check('fecha_de_nacimiento','fecha de nacimiento debe ser valida')
+       .exists()
+       .isDate(),
+    check('contrasenia','La clave debe tener almenos 5 caracteres')
+       .exists()
+       .isLength({min:5})
+                
+
+
+
+], (req,res,next)=>{
+    const error = validationResult(req)
+    const validaciones = error.array();//arreglo con errores
+    const valores = req.body
+    if(!error.isEmpty()){
+        //return res.status(400).json({errors: error.array()});
+        res.render('../views/crear_cuenta.ejs',{validaciones:validaciones,valores:valores})
+    }else{
+        res.json('verificado!')
+        next()
+
+        
+    } 
+    
+}, create_user)
+
+//Bsucar email Usuario
+router_web.post('/login',urlencoded,user_session_validator)
+
+//Cookies for logged user
+router_web.get('loged_user',)
+
+//Borrar cuenta
+router_web.delete('/usuarios/:id', deleteOne)
+//await fetch('http://localhost:3000/register')
+
+// async function buscar(){
+//     const response = await fetch('http://localhost:3000/register')
+//     const json = await response.json()
+// }
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+//Ruta para agregar el usuario 
+//router_web.post('/register',create_user)
+
+module.exports = router_web
---
+++ sprint_iv_trabajo_i/controllers/controllers.js
@@ -0,0 +1,113 @@
+const {read_products,read_one_product,add_user,user_filter} = require('../services/crud');
+const bcrypt = require('bcrypt')
+const cookieParser = require('cookie-parser')
+const{sequelize} = require('sequelize');
+const usuarios = require('../models').usuarios
+const productos = require('../models').productos
+//Obtenemos todos los productos
+const getall = (req,res)=>{
+    return productos.findAll()
+    .then(productos => res.status(200).send(productos))
+    .catch(error => res.status(404).send(error))
+
+}
+//Obtenemos un solo producto
+const getone = (req,res)=>{
+    const{id}=req.params;
+    
+    return productos.findOne({where:{id:id}})
+    .then(products => res.status(200).send(products))
+    .catch(error => res.status(404).send(error))
+    
+}
+
+const create_product = (req,res)=>{
+    const {nombre,descripcion,precio} = req.body
+    console.log({nombre,descripcion,precio})
+    return productos.create({nombre:nombre,descripcion:descripcion,precio:precio})
+    .then(products => res.status(201).send(products))
+    .catch(error => res.status(500).send(error))
+}
+
+const delete_product = async(req,res)=>{
+    const {id} = req.params
+    const product = await productos.findOne({where:{id:id}})
+    if (!product){
+        return res.status(404).json({ error: 'no se encuentra'})
+    }else{
+        return productos.destroy({
+            where: { id: id}
+        })
+        .then(productos => res.status(201).send('Deleted'))
+        .catch(error => res.status(500).send(error))
+    } 
+    
+    
+    
+}
+
+//Creamos un nuevo usuario en la base de datos
+const create_user = (req,res)=>{
+    const {nombre,apellido,correo,fecha_de_nacimiento,contrasenia} = req.body
+    //Encriptado de clave///////////////////////////////////////////////////
+    const clave_hash = bcrypt.hashSync(contrasenia,8)
+    
+    ////////////////////////////////////////////////////////////////////////
+    
+    
+    //Creacion exitosa
+    return usuarios.create({nombre,apellido,correo,fecha_de_nacimiento,contrasenia:clave_hash})
+    
+}
+
+//Buscador de usuario por email
+const user_session_validator = async(req,res)=>{
+    const datos = req.body;
+    const email = datos.email;
+    const clave = datos.pwd
+
+    //const [boleano,key] = user_filter(email);
+    const user = await usuarios.findOne({where:{correo:email}})
+    
+
+    if(!user){
+        return res.status(404).json({ error: 'no se encuentra'})
+    }else{
+        const key = user_filter(email)
+        res.cookie('email_user',email,{
+        maxage: 15*60*1000
+        })
+        res.cookie('clave',key,{
+        maxage: 1*60*1000
+        })
+        //res.status(200).json({msg:'Usuario logueado'})
+        res.render('../views/desktop_principal.ejs')
+    }
+
+
+}
+
+const middleware_cookie = (req,res)=>{
+    const datos = req.body;
+    const email = datos.email;
+    const clave = datos.pwd
+    res.cookie('email_user',email,{
+        maxage: 15*60*1000
+    })
+}
+
+const deleteOne =  (req, res) => {
+    const { id } = req.params
+    const usuario = usuarios.findOne(
+        {   where: { id: id} })
+        
+    if (!usuario) return res.status(404).json({ error: 'no se encuentra'})
+    
+    return usuarios.destroy({
+        where: { id: id}
+    })
+    .then(usuarios => res.status(201).send(usuario))
+    .catch(error => res.status(500).send(error))
+}
+
+module.exports = {getall,getone,create_user,user_session_validator,middleware_cookie,deleteOne,create_product,delete_product}
